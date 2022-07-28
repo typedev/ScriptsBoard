8645c2a3c2849da89276cb04b9f2b7829ded0dfb
Alexander Lubovenko <lubovenko@me.com>
2022-07-29 02:02:52 +0300

    fixed bug - not loaded scripts when Inspector shows

--- source/ScriptsBoard.py
+++ source/ScriptsBoard.py
@@ -1,32 +1,25 @@
 """
-ScriptBoard is an extension for quickly launching your favorite scripts. 
+ScriptsBoard is an extension for quickly launching your favorite scripts.
 In order for scripts to run, they must follow this format:
 ```
-	__doc__ = 'description'
+__doc__ = 'description' # optional
 
-	def main():
-		print ("place your code here")
+def main():
+	print ("place your code here")
 
-	if __name__ == "__main__":
-		main()
+if __name__ == "__main__":
+	main()
 
 ```
+
 """
 
-import os, sys, importlib, string, random
-# from AppKit import *
-from mojo.UI import *
+import os, sys, importlib
 from vanilla import *
 from vanilla.dialogs import getFile
-# from plistlib import readPlist
 
 from mojo.subscriber import Subscriber, registerRoboFontSubscriber
 from mojo.extensions import getExtensionDefault, setExtensionDefault
-
-# import importlib
-# import tdLibEssentials
-# importlib.reload(tdLibEssentials)
-# from tdLibEssentials import *
 
 
 class ScriptBoardSettings(object):
@@ -44,33 +37,23 @@ class ScriptBoardSettings(object):
 
 	def get (self, key):
 		try:
-			return self.data[key]
+			return list(self.data[key])
 		except:
 			return None
 
 	def set (self, key, value):
-		self.data[key] = value
+		self.data[key] = tuple(value)
+		self.save()
 
-def getUniqName(cut=32):
-	def ran_gen (size, chars=string.ascii_uppercase + string.digits):
-		return ''.join(random.choice(chars) for x in range(size))
-	return 'uuid' + ran_gen(cut, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 
 class ScriptsBoardEmbedded(Subscriber):
 	debug = True
 
 	def build (self):
-		# self.editor = TextEditor((10, 10, -10, -0))
-
-		_version = '0.6'
-		# self.parent = parent
-		# if not parent:
+		_version = '0.7'
 		_baseDefaultKey = "com.typedev.ScriptBoard.v%s." % _version
 		_dataDefaultKey = "%s.data" % _baseDefaultKey
-		# else:
-		# 	_baseDefaultKey = "com.typedev.ScriptBoard.v%s.%s" % (_version, self.parent.idName)
-		# 	_dataDefaultKey = "%s.data" % _baseDefaultKey
 
 		settings = dict(
 			_baseDefaultKey = _baseDefaultKey,
@@ -79,14 +62,8 @@ class ScriptsBoardEmbedded(Subscriber):
 
 		self._prefs = ScriptBoardSettings(settings)
 		self._prefs.load()
-		# self._pos = self._prefs.get('pos')
-		# x, y, w, h = self._pos
-		self.w = Group((0,0,-0,-0)) #,minSize = (100, 200), title = 'Scripts Board %s' % _version
-		# self.w.setPosSize(self._pos)
+		self.w = Group((0,0,-0,-0))
 		self.w.flex = Group('auto')
-		# self.w.g2 = Group('auto')
-		# self.w.g3 = Group('auto')
-		# self.w.g4 = Group('auto')
 		self.w.btnAdd = Button('auto',
 		                       title = '+',
 		                       callback = self.btnAddCallback,
@@ -111,8 +88,6 @@ class ScriptsBoardEmbedded(Subscriber):
 		rules = [
 			# Horizontal
 			"H:|-border-[flex]-border-[btnAdd]-border-[btnDel(==btnAdd)]-border-|",
-			# "H:|-border-[btnAdd]-border-[btnDel(==btnAdd)]-border-|",
-
 			"H:|-0-[scriptsListing]-0-|",
 			"H:|-0-[textBox]-0-|",
 			"H:|-border-[btnRun]-border-|",
@@ -127,90 +102,71 @@ class ScriptsBoardEmbedded(Subscriber):
 		}
 		self.w.addAutoPosSizeRules(rules, metrics)
 
-		# paneDescriptors = [
-		# 	dict(view = self.w.scriptsListing, identifier = "pane1", minSize = (100), canCollapse = False),
-		# 	dict(view = self.w.textBox, identifier = "pane2", minSize = (100), canCollapse = False),
-		# ]
-		# self.w.splitView = SplitView((0, 30, -0, -32), paneDescriptors,
-		#                                      isVertical = False,
-		#                                      dividerStyle = 'thin',
-		#                                      dividerThickness = 5)
-
-
-		# self.w.bind("close", self.windowClose)
+	def started(self):
 		self.loadScriptsList()
-		# self.w.open()
 
 
 	def roboFontWantsInspectorViews (self, info):
 		registerRoboFontSubscriber(ScriptsBoardEmbedded)
-		# create an inspector item
-		item = dict(label = "Scripts Board", view = self.w, minSize=300, size=300,collapsed=True, canResize=True)
-		# insert or append the item to the list of inspector panes
+		item = dict(label = "Scripts Board", view = self.w, minSize=200, size=300, collapsed=True, canResize=True)
 		info["viewDescriptions"].insert(0, item)
-
-
-
-	# def __init__(self, parent = None):
-
-
-	#
-	# def windowClose(self, sender):
-	# 	# self._prefs.set('pos',self.w.getPosSize())
-	# 	self._prefs.save()
+		self.loadScriptsList()
 
 
 	def loadScriptsList(self):
-		slist = []
 		self.w.scriptsListing.set([])
 		slist = self._prefs.get('scripts')
-		todelete = []
+		# reset = False
+		# if reset:
+		# 	self._prefs.set('scripts',[])
+		toDelete = []
+		toSet = []
 		for scriptdata in slist:
-			idname, name, path = scriptdata
+			name, path = scriptdata
 			if os.path.exists(path):
-				self.w.scriptsListing.append(name)
+				toSet.append(name)
 			else:
-				todelete.append(idname)
-		if todelete:
-			for idname in todelete:
-				print('Script: %s path: %s not founded..')
+				toDelete.append((name, path))
+		self.w.scriptsListing.set(toSet)
+		if toDelete:
+			for idname in toDelete:
+				print('ScriptsBoard:',idname,' not found..')
 				self.deleteScriptFromPrefs(idname = idname)
 
 
 	def addScriptToList(self, path):
-		nameraw = path.split('/')[-1]
-		name, ext = nameraw.split('.')
+		namefile = os.path.basename(path)
+		name, ext = namefile.split('.')
 		slist = self._prefs.get('scripts')
-		if ext == 'py':
-			slist.append((getUniqName(),name,path))
+		if ext == 'py' and (name, path) not in slist:
+			slist.append((name, path))
+			self._prefs.set('scripts', slist)
 			self.loadScriptsList()
-			self._prefs.save()
-		elif ext == 'roboFontExt':
-			pass
-			# print 'extension', name
-			# plist = readPlist('%s/info.plist' % path)
-			# name_ext = plist['name']
-			# for items in plist['addToMenu']:
-			# 	# for key, value in items.items():
-			# 	py_path = '%s/lib/%s' % (path, items['path'])
-			# 	name_script = items['preferredName']
-			# 	# print name_ext, name_script, py_path
-			# 	s.append(('%s.%s' % (name_ext, name_script),py_path))
-			# self.loadScriptsList()
+		# elif ext == 'roboFontExt':
+		# 	pass
+		# 	# print 'extension', name
+		# 	# plist = readPlist('%s/info.plist' % path)
+		# 	# name_ext = plist['name']
+		# 	# for items in plist['addToMenu']:
+		# 	# 	# for key, value in items.items():
+		# 	# 	py_path = '%s/lib/%s' % (path, items['path'])
+		# 	# 	name_script = items['preferredName']
+		# 	# 	# print name_ext, name_script, py_path
+		# 	# 	s.append(('%s.%s' % (name_ext, name_script),py_path))
+		# 	# self.loadScriptsList()
 		else:
 			print ('Wrong file format!..')
 
 
 	def scriptsListDblClickCallback(self, sender):
 		idx = self.w.scriptsListing.getSelection()
-		idname, name, path = self._prefs.get('scripts')[idx[0]]
+		name, path = self._prefs.get('scripts')[idx[0]]
 		path = path.replace('%s.py' % name, '')
 		print ('Running',name,path)
-		sys.path.append(path)
+		if path not in sys.path:
+			sys.path.append(path)
 		m = importlib.import_module(name)
 		importlib.reload(m)
-		# if self.parent and hasattr(m, 'main'):
-		# 	m.main(self.parent)
 		if hasattr(m, 'main'):
 			m.main()
 		else:
@@ -222,9 +178,10 @@ class ScriptsBoardEmbedded(Subscriber):
 	def scriptsListSelectionCallback(self, sender):
 		idx = sender.getSelection()
 		if not idx: return
-		idname, name, scriptpath = self._prefs.get('scripts')[idx[0]]
+		name, scriptpath = self._prefs.get('scripts')[idx[0]]
 		path = scriptpath.replace('%s.py' % name, '')
-		sys.path.append(path)
+		if path not in sys.path:
+			sys.path.append(path)
 		m = importlib.import_module(name)
 		importlib.reload(m)
 		txt = ''
@@ -239,30 +196,27 @@ class ScriptsBoardEmbedded(Subscriber):
 		if paths:
 			for path in paths:
 				self.addScriptToList( path )
-			self._prefs.save()
 
 
 	def deleteScriptFromPrefs(self, idname):
 		slist = self._prefs.get('scripts')
 		for idx, scriptdata in enumerate(slist):
-			_idname, _name, path = scriptdata
-			if _idname == idname:
+			_name, path = scriptdata
+			if (_name, path) == idname:
 				del slist[idx]
-				self._prefs.save()
+				self._prefs.set('scripts', slist)
 				break
 		self.loadScriptsList()
 
 
 	def btnDelCallback(self, sender):
 		idx = self.w.scriptsListing.getSelection()
-		idname, name, path = self._prefs.get('scripts')[idx[0]]
-		self.deleteScriptFromPrefs(idname = idname)
-		self._prefs.save()
+		name, path = self._prefs.get('scripts')[idx[0]]
+		self.deleteScriptFromPrefs(idname = (name, path))
 
 
-def main (parent = None):
+def main ():
 	registerRoboFontSubscriber(ScriptsBoardEmbedded)
-	# ScriptsBoardEmbedded(parent)
 
 
 if __name__ == "__main__":
